ddb63f3e779ffe76ee1950eb61b4b38bbf174081
siegfried bozza <siegfried.bozza@protonmail.com>
2022-12-03 19:03:19 +0100

    Add 01-basic-nft.deploy.js script

---
+++ deploy/01-basic-nft.deploy.js
@@ -0,0 +1,39 @@
+const { config, network } = require("hardhat")
+const {
+  networkConfig,
+  developmentChains,
+} = require("../helper-hardhat-config.js")
+const { verify } = require("../utils/verify")
+
+module.exports = async (hre) => {
+  const { getNamedAccounts, deployments } = hre
+  const { deploy, log } = deployments
+  const { deployer } = await getNamedAccounts()
+  const chainId = network.config.chainId
+  const args = []
+
+  console.log("---------")
+  console.log("Deploying BasicNFT...")
+
+  const basicNft = await deploy("BasicNFT", {
+    contract: "BasicNFT",
+    from: deployer,
+    args: args,
+    log: true,
+    waitConfirmations: network.config.blockConfirmations || 1,
+  })
+  console.log("Deploying BasicNFT...DONE")
+  console.log(`Contract deployed to ${basicNft.address}`)
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    console.log(`Contract EtherScan Verification...`)
+    await verify(basicNft.address, args)
+  }
+
+  console.log("---------")
+}
+
+module.exports.tags = ["all", "basicNft"]
