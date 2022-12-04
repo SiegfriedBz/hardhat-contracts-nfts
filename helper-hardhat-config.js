31cdec3887360c5ba4b8b2d0b4a0186eecc348bc
siegfried bozza <siegfried.bozza@protonmail.com>
2022-12-04 15:59:22 +0100

    Update helper-hardhat-config

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -16,7 +16,7 @@ const networkConfig = {
       "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
     callbackGasLimit: "500000", // 500,000 gas
     mintFee: "10000000000000000", // 0.01 ETH
-    subscriptionId: "5772", // add your ID here!
+    subscriptionId: "5772",
   },
 }
 
