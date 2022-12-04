c411c98ade7e8399c9b26661416199cfe60dc384
siegfried bozza <siegfried.bozza@protonmail.com>
2022-12-04 16:53:24 +0100

    Update hh config

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -2,10 +2,14 @@ const networkConfig = {
   31337: {
     name: "localhost",
     ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
+    //vrfCoordinatorV2: use Mock on local blockchain
     gasLane:
       "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
     mintFee: "10000000000000000", // 0.01 ETH
     callbackGasLimit: "500000", // 500,000 gas
+    // subscriptionId: done programmatically on local blockchain
+    link_baseFee: ethers.utils.parseEther("0.25"), //it costs 0.25LINK per rdom number request// uint96 _baseFee : "Premium" value, network-specific at https://docs.chain.link/docs/vrf/v2/subscription/supported-networks/#goerli-testnet
+    link_gasPriceLink: 10e9, // ~ LINK per GAS
   },
   // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
   5: {
@@ -14,9 +18,11 @@ const networkConfig = {
     vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
     gasLane:
       "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
-    callbackGasLimit: "500000", // 500,000 gas
     mintFee: "10000000000000000", // 0.01 ETH
+    callbackGasLimit: "500000", // 500,000 gas
     subscriptionId: "5772",
+    link_baseFee: ethers.utils.parseEther("0.25"),
+    link_gasPriceLink: 10e9,
   },
 }
 
